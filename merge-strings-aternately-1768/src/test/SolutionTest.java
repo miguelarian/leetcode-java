f54f922b4c74bf1351643e70c8ee578c66c8c267
miguelarian <miguelvr88@gmail.com>
2024-02-28 19:57:53 +0100

    add test: empty word1 and no empty word2

--- merge-strings-aternately-1768/src/test/SolutionTest.java
+++ merge-strings-aternately-1768/src/test/SolutionTest.java
@@ -10,4 +10,13 @@ public class SolutionTest {
 
         Assertions.assertEquals("", result);
     }
+
+    @Test
+    public void emptyWord1_and_noEmptyWord2_return_word2() {
+        String word1 = "", word2 = "123";
+
+        String result = Solution.mergeAlternately(word1, word2);
+
+        Assertions.assertEquals("123", result);
+    }
 }
