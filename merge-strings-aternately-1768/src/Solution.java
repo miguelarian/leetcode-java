9f24c8d78f099b9d560bd961b3c5c934dd66d0f6
miguelarian <miguelvr88@gmail.com>
2024-02-28 19:58:45 +0100

    add code to pass emptyWord1_and_noEmptyWord2_return_word2

--- merge-strings-aternately-1768/src/Solution.java
+++ merge-strings-aternately-1768/src/Solution.java
@@ -1,5 +1,9 @@
 public class Solution {
     public static String mergeAlternately(String word1, String word2) {
+
+        if(word1 == "") {
+            return word2;
+        }
         return "";
     }
 }
