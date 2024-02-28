4c7af8ffd3353a1b0fbb0d6b7621b60c4ba473d8
miguelarian <miguelvr88@gmail.com>
2024-02-28 19:55:52 +0100

    add fist test: empty words

--- merge-strings-aternately-1768/src/test/SolutionTest.java
+++ merge-strings-aternately-1768/src/test/SolutionTest.java
@@ -2,9 +2,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
-
     @Test
-    public void testMergeAlternately() {
-        Assertions.fail("forced to fail");
+    public void emptyWords_return_empty() {
+        String word1 = "", word2 = "";
+
+        String result = Solution.mergeAlternately(word1, word2);
+
+        Assertions.assertEquals("", result);
     }
 }
