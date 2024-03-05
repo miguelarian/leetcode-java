2ad6e88794c5ee5730f023169af624fa6f46a795
miguelarian <miguelvr88@gmail.com>
2024-03-05 20:20:32 +0100

    add problem two-sum-1

---
+++ two-sum-1/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println("Solution: " + Arrays.toString(s.twoSum(new int[]{2, 7, 11, 16}, 9)));
+    }
+}
