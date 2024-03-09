aed229e95e0acc76d351d81c1a622066bd1d0f3a
miguelarian <miguelvr88@gmail.com>
2024-03-09 12:15:20 +0100

    add test for 1 element rotation

--- RotateArray-189/src/main/java/com/miguelvela/Solution.java
+++ RotateArray-189/src/main/java/com/miguelvela/Solution.java
@@ -1,7 +1,15 @@
 package com.miguelvela;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void rotate(int[] nums, int k) {
 
+        int[] splitArrayPart1 = Arrays.copyOfRange(nums, 0, nums.length-k);
+        int[] splitArrayPart2 = Arrays.copyOfRange(nums, nums.length-k, nums.length);
+
+        System.arraycopy(splitArrayPart1, 0, nums, splitArrayPart2.length, splitArrayPart1.length);
+        System.arraycopy(splitArrayPart2, 0, nums, 0, splitArrayPart2.length);
+        System.out.println(Arrays.toString(nums));
     }
 }
--- RotateArray-189/src/test/java/com/miguelvela/SolutionTest.java
+++ RotateArray-189/src/test/java/com/miguelvela/SolutionTest.java
@@ -7,7 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SolutionTest {
 
     @Test
-    public void rotate() {
-        fail("test not implemented");
+    public void rotate_Kis0_noRotation() {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int k = 0;
+
+        Solution.rotate(nums, k);
+        int[] expected = {1,2,3,4,5,6,7,8,9,10};
+
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void rotate_Kis1_rotated1position() {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int k = 1;
+
+        Solution.rotate(nums, k);
+        int[] expected = {10,1,2,3,4,5,6,7,8,9};
+
+        assertArrayEquals(expected, nums);
     }
 }
