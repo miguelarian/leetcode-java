0e87e92f2f4d51faf9a6fe0586657c780e3a1fcf
miguelarian <miguelvr88@gmail.com>
2024-03-09 12:19:26 +0100

    add test for large K

--- RotateArray-189/src/main/java/com/miguelvela/Solution.java
+++ RotateArray-189/src/main/java/com/miguelvela/Solution.java
@@ -5,11 +5,14 @@ import java.util.Arrays;
 public class Solution {
     public static void rotate(int[] nums, int k) {
 
-        int[] splitArrayPart1 = Arrays.copyOfRange(nums, 0, nums.length-k);
-        int[] splitArrayPart2 = Arrays.copyOfRange(nums, nums.length-k, nums.length);
+        int sanitizedK = k % nums.length;
+
+        int[] splitArrayPart1 = Arrays.copyOfRange(nums, 0, nums.length-sanitizedK);
+        int[] splitArrayPart2 = Arrays.copyOfRange(nums, nums.length-sanitizedK, nums.length);
 
         System.arraycopy(splitArrayPart1, 0, nums, splitArrayPart2.length, splitArrayPart1.length);
         System.arraycopy(splitArrayPart2, 0, nums, 0, splitArrayPart2.length);
+
         System.out.println(Arrays.toString(nums));
     }
 }
--- RotateArray-189/src/test/java/com/miguelvela/SolutionTest.java
+++ RotateArray-189/src/test/java/com/miguelvela/SolutionTest.java
@@ -27,4 +27,26 @@ public class SolutionTest {
 
         assertArrayEquals(expected, nums);
     }
+
+    @Test
+    public void rotate_KisNumsLenght_originalNums() {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int k = nums.length;
+
+        Solution.rotate(nums, k);
+        int[] expected = {1,2,3,4,5,6,7,8,9,10};
+
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    public void rotate_KisBiggerThanNumsLenght_originalNums() {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int k = nums.length + 1;
+
+        Solution.rotate(nums, k);
+        int[] expected = {10,1,2,3,4,5,6,7,8,9};
+
+        assertArrayEquals(expected, nums);
+    }
 }
