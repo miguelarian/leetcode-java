4d65528439158353216d38c6cd02259079a68172
miguelarian <miguelvr88@gmail.com>
2024-03-11 16:05:13 +0100

    refactor to use an array with the modules

--- FizzBuzz/src/main/java/com/miguelvela/Solution.java
+++ FizzBuzz/src/main/java/com/miguelvela/Solution.java
@@ -1,29 +1,35 @@
 package com.miguelvela;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution {
     public static List<String> fizzBuzz(int n) {
 
+        HashMap<Integer, String> modules = new HashMap<>();
+        modules.put(3, "Fizz");
+        modules.put(5, "Buzz");
+
         List<String> result = new ArrayList<>();
-        for (int number = 1; number <= n; number++) {
-            if(number % 3 == 0 && number % 5 == 0)
-            {
-                result.add("FizzBuzz");
+        for (int number = 1; number <= n; number++)
+        {
+            StringBuilder wordBuilder = new StringBuilder();
+
+            for (Map.Entry<Integer, String> entry : modules.entrySet()) {
+                int module = entry.getKey();
+
+                if(number % module == 0) {
+                    wordBuilder.append(entry.getValue());
+                }
             }
-            else if(number % 3 == 0)
+            if(wordBuilder.isEmpty())
             {
-                result.add("Fizz");
+                wordBuilder.append(number);
             }
-            else if(number % 5 == 0)
-            {
-                result.add("Buzz");
-            }
-            else
-            {
-                result.add(String.valueOf(number));
-            }
+
+            result.add(wordBuilder.toString());
         }
 
         return result;
