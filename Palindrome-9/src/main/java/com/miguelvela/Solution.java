0016ed708960791a3619e62633e69e195e712223
miguelarian <miguelvr88@gmail.com>
2024-03-12 16:29:31 +0100

    add basic test for single digit input

--- Palindrome-9/src/main/java/com/miguelvela/Solution.java
+++ Palindrome-9/src/main/java/com/miguelvela/Solution.java
@@ -2,7 +2,12 @@ package com.miguelvela;
 
 public class Solution {
 
-    public static boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int number) {
+
+        if (number < 10) {
+            return true;
+        }
+
         return false;
     }
 }
--- Palindrome-9/src/test/java/com/miguelvela/SolutionTest.java
+++ Palindrome-9/src/test/java/com/miguelvela/SolutionTest.java
@@ -1,13 +1,20 @@
 package com.miguelvela;
 
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
+import static com.miguelvela.Solution.isPalindrome;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class SolutionTest {
 
     @Test
-    void isPalindrome() {
-        fail();
+    void isPalindrome_singleDigit_returnsTrue() {
+        int number = 1;
+
+        boolean result = isPalindrome(number);
+
+        assertThat("The number should be palindrome", result, Is.is(true));
     }
 }
