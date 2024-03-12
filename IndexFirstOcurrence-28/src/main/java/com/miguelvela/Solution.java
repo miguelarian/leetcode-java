de3e4350a473f775bc70afc58b1d9425f1d063d9
miguelarian <miguelvr88@gmail.com>
2024-03-12 20:19:47 +0100

    add not present and same word tests

--- IndexFirstOcurrence-28/src/main/java/com/miguelvela/Solution.java
+++ IndexFirstOcurrence-28/src/main/java/com/miguelvela/Solution.java
@@ -2,7 +2,16 @@ package com.miguelvela;
 
 public class Solution {
     public static int strStr(String haystack, String needle) {
+        // AVOID: return haystack.indexOf(needle);
 
-        return 0;
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+        if(needle.equals(haystack)) {
+            return 0;
+        }
+
+
+        return -1;
     }
 }
--- IndexFirstOcurrence-28/src/test/java/com/miguelvela/SolutionTest.java
+++ IndexFirstOcurrence-28/src/test/java/com/miguelvela/SolutionTest.java
@@ -3,12 +3,30 @@ package com.miguelvela;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
+import static com.miguelvela.Solution.strStr;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SolutionTest {
 
     @Test
-    public void strStr() {
-        assertThat(false, Is.is(true));
+    public void strStr_needle_not_present_minusOne() {
+
+        String haystack = "this_is_a_test";
+        String needle = "none";
+
+        int index = strStr(haystack, needle);
+
+        assertThat("The needle is not present", index, Is.is(-1));
+    }
+
+    @Test
+    public void strStr_needle_haystack_same_word_zero() {
+
+        String haystack = "this_is_a_test";
+        String needle = "this_is_a_test";
+
+        int index = strStr(haystack, needle);
+
+        assertThat("The needle is present", index, Is.is(0));
     }
 }
