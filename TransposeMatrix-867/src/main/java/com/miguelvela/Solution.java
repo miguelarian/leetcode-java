7358072bae0021006fceecbfd720ac5e55532e2f
miguelarian <miguelvr88@gmail.com>
2024-03-12 19:57:41 +0100

    add basic 1x1 test

--- TransposeMatrix-867/src/main/java/com/miguelvela/Solution.java
+++ TransposeMatrix-867/src/main/java/com/miguelvela/Solution.java
@@ -3,6 +3,20 @@ package com.miguelvela;
 public class Solution {
     public static int[][] transpose(int[][] matrix) {
 
-        return null;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] result = new int[columns][rows];
+
+        for(int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                int value = matrix[row][column];
+                result[column][row] = value;
+            }
+        }
+
+        return result;
     }
 }
