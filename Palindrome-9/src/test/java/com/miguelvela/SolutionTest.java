5019e6bd4a3d4fafc420aef59d8a55a6848bea93
miguelarian <miguelvr88@gmail.com>
2024-03-12 16:42:25 +0100

    add more test cases

--- Palindrome-9/src/test/java/com/miguelvela/SolutionTest.java
+++ Palindrome-9/src/test/java/com/miguelvela/SolutionTest.java
@@ -23,12 +23,39 @@ class SolutionTest {
 
         boolean result = isPalindrome(number);
 
-        assertThat("The number should be palindrome", result, Is.is(false));
+        assertThat("The number should not be palindrome", result, Is.is(false));
     }
 
     @Test
     void isPalindrome_twoEqualDigits_returnsTrue() {
         int number = 11;
+
+        boolean result = isPalindrome(number);
+
+        assertThat("The number should be palindrome", result, Is.is(true));
+    }
+
+    @Test
+    void isPalindrome_twoNegativeEqualDigits_returnsTrue() {
+        int number = -11;
+
+        boolean result = isPalindrome(number);
+
+        assertThat("The number should not be palindrome", result, Is.is(false));
+    }
+
+    @Test
+    void isPalindrome_longPalindrome_returnsTrue() {
+        int number = 1122332211;
+
+        boolean result = isPalindrome(number);
+
+        assertThat("The number should be palindrome", result, Is.is(true));
+    }
+
+    @Test
+    void isPalindrome_oddDigitsLongPalindrome_returnsTrue() {
+        int number = 112232211;
 
         boolean result = isPalindrome(number);
 
