d4b2812c87f44e7edb54f4cdb060e4508983b0ae
miguelarian <miguelvr88@gmail.com>
2024-03-12 16:30:38 +0100

    add negative single digit input test

--- Palindrome-9/src/test/java/com/miguelvela/SolutionTest.java
+++ Palindrome-9/src/test/java/com/miguelvela/SolutionTest.java
@@ -5,13 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import static com.miguelvela.Solution.isPalindrome;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 
 class SolutionTest {
 
     @Test
     void isPalindrome_singleDigit_returnsTrue() {
         int number = 1;
+
+        boolean result = isPalindrome(number);
+
+        assertThat("The number should be palindrome", result, Is.is(true));
+    }
+
+    @Test
+    void isPalindrome_singleNegativeDigit_returnsTrue() {
+        int number = -1;
 
         boolean result = isPalindrome(number);
 
