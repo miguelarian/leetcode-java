2cfebdc4509ac814b18b37175155de4642047103
miguelarian <miguelvr88@gmail.com>
2024-07-11 13:02:09 +0200

    remove comments

--- JumpGame-55/src/main/java/com/miguelvela/Solution.java
+++ JumpGame-55/src/main/java/com/miguelvela/Solution.java
@@ -28,23 +28,3 @@ public class Solution {
         return false;
     }
 }
-
-//  [] -> true
-//  [0] -> true
-//  [99] -> true
-//  [1,1] -> true
-//  [0,x] -> false -> no llego al final
-//  [1,x] -> true -> 1 < length del resto del array
-//  [2,x] -> false -> me salgo del array
-
-//  [1,0,x] -> false -> no llego
-//  [2,0,x] -> true -> llega
-//  [3,0,x] -> true -> porque los anteriores dieron true
-//  [y,0,x] -> true...
-
-//  [0,y,x] -> false siempre
-//  [1,0,x] -> false => 1 + [0,x] => false
-
-// go from right to left, and check if the problem is already solved
-
-// use recursion
